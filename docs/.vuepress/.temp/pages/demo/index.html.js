9570d3073f375a82fc9694b9a34139424eafc860
yhomma-fa <homma.yoshitaka@fastaccounting.co.jp>
2023-03-15 15:59:53 +0900

    first-commit

--- docs/.vuepress/.temp/pages/demo/index.html.js
+++ docs/.vuepress/.temp/pages/demo/index.html.js
@@ -1,14 +1,1 @@
-export const data = JSON.parse("{\"key\":\"v-1473bf53\",\"path\":\"/demo/\",\"title\":\"Demo Page\",\"lang\":\"ja\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"Pages\",\"slug\":\"pages\",\"link\":\"#pages\",\"children\":[]}],\"git\":{\"updatedTime\":null,\"contributors\":[]},\"filePathRelative\":\"demo/README.md\"}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
+export const data = JSON.parse("{\"key\":\"v-1473bf53\",\"path\":\"/demo/\",\"title\":\"Demo Page\",\"lang\":\"ja\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"Pages\",\"slug\":\"pages\",\"link\":\"#pages\",\"children\":[]}],\"git\":{\"updatedTime\":1678860312000,\"contributors\":[{\"name\":\"yhomma-fa\",\"email\":\"homma.yoshitaka@fastaccounting.co.jp\",\"commits\":1}]},\"filePathRelative\":\"demo/README.md\"}")
--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -1,14 +1,1 @@
-export const data = JSON.parse("{\"key\":\"v-8daa1a0e\",\"path\":\"/\",\"title\":\"API Page Sample\",\"lang\":\"ja\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"Contents\",\"slug\":\"contents\",\"link\":\"#contents\",\"children\":[{\"level\":3,\"title\":\"Demo\",\"slug\":\"demo\",\"link\":\"#demo\",\"children\":[]}]}],\"git\":{\"updatedTime\":null,\"contributors\":[]},\"filePathRelative\":\"README.md\"}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
+export const data = JSON.parse("{\"key\":\"v-8daa1a0e\",\"path\":\"/\",\"title\":\"API Page Sample\",\"lang\":\"ja\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"Contents\",\"slug\":\"contents\",\"link\":\"#contents\",\"children\":[{\"level\":3,\"title\":\"Demo\",\"slug\":\"demo\",\"link\":\"#demo\",\"children\":[]}]}],\"git\":{\"updatedTime\":1678860312000,\"contributors\":[{\"name\":\"yhomma-fa\",\"email\":\"homma.yoshitaka@fastaccounting.co.jp\",\"commits\":1}]},\"filePathRelative\":\"README.md\"}")
